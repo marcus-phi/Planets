f044e6fe8ef5bb3103bd89ddc1e08ef6149a161d
Nhat Phi <phixuannhat@gmail.com>
2020-02-03 00:13:18 -0600

    add detail page with hero animation

--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -1,4 +1,5 @@
 import 'package:Planets/podo/Planet.dart';
+import 'package:Planets/ui/detail/DetailPage.dart';
 import 'package:flutter/material.dart';
 
 class PlanetRow extends StatelessWidget {
@@ -11,11 +12,14 @@ class PlanetRow extends StatelessWidget {
     final planetThumbnail = new Container(
       margin: new EdgeInsets.symmetric(vertical: 16.0),
       alignment: FractionalOffset.centerLeft,
-      child: new Image(
-        image: new AssetImage(planet.image),
-        height: 92.0,
-        width: 92.0,
-      ),
+      child: new Hero(
+        tag: 'planet-hero-${planet.id}',
+        child: new Image(
+          image: new AssetImage(planet.image),
+          height: 92.0,
+          width: 92.0,
+        ),
+      )
     );
 
     final baseTextStyle = const TextStyle(
@@ -88,17 +92,23 @@ class PlanetRow extends StatelessWidget {
       child: planetCardContent,
     );
 
-    return new Container(
-      margin: const EdgeInsets.symmetric(
-        vertical: 16.0,
-        horizontal: 24.0,
-      ),
-      child: new Stack(
-        children: <Widget>[
-          planetCard,
-          planetThumbnail,
-        ],
-      ),
+    return new GestureDetector(
+      onTap: () => Navigator.of(context).push(new PageRouteBuilder(
+        pageBuilder: (_, __, ___) => new DetailPage(planet)
+      )),
+      child: new Container(
+        height: 120.0,
+        margin: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 24.0,
+        ),
+        child: new Stack(
+          children: <Widget>[
+            planetCard,
+            planetThumbnail,
+          ],
+        ),
+      )
     );
   }
 }
